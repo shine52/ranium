b3e50923b60c599cd4ca3b153a720790e625bebe
suns.shine <suns.shine@5253@gmail.com>
2022-05-14 21:07:14 +0530

    computations done

--- app/Http/Controllers/neoController.php
+++ app/Http/Controllers/neoController.php
@@ -17,13 +17,80 @@ class neoController extends Controller
         $end_date = $request->input('end_date');
         $apiUrl = "https://api.nasa.gov/neo/rest/v1/feed?start_date=".$start_date. "&end_date=".$end_date."&api_key=DEMO_KEY";
 
-        // $neoData = Http::get($apiUrl);
+        //get data from external api
+        $neoData = Http::get($apiUrl);
+
+        //get the total asteroids between the date range ,from the data
+        $element_count => $neoData['element_count'];
+        
+        //create a collection of the relevant data i.e 'near_earth_objects' 
+        $neoDayData = collect($neoData['near_earth_objects']); 
+
+        //process the collection in the form :
+        // [[id=>1,date=>'2022-5-1',size=>35,distance=>1000],[..],[..]]
+        
+        //initialize an empty collection of asteroids
+        $asteroidList = collect([]);
+        
+        // total size of all the asteroids between the given dates
+        $totalSizeKms = 0;
+        
+
+        $neoDayData->each(function($asteroids,$date){
+            $asteroidsCollection = collect($asteroids);
+            
+            //get each days count,to generate graph
+            $dayCount= array(
+                'date'=> $date,
+                'dayCount'=>$asteroidsCollection->count()    
+            );
+            
+            //generate graph data
+            $graphData->push($dayCount);
+            
+            $asteroidsCollection->each(function($asteroidData,$key){
+                $id = $asteroidData->id;
+                $speedKmph = $asteroidData->kilometers_per_hour;
+                $distanceKms = $asteroidData->kilometers;
+                $approach_date = $asteroidData->close_approach_date;
+                $sizeKms = $asteroidData->estimated_diameter[0];
+                
+                $totalSizeKms += $sizeKms;
+                
+                //create asteroid object
+                $asteroid = array(
+                    'id'=>$id,
+                    'speedKmph'=>$speedKmph,
+                    'distanceKms'=>$distanceKms,
+                    'approach_date'=>$approach_date,
+                    'sizeKms'=>$sizeKms,
+                );
+                
+                //add the asteroid object to asteroid list
+                $asteroidList->push($asteroid);
+            });
+        });        
+
+        //compute the average size
+        $averageSizeKms = $totalSizeKms/$element_count;
+
         // return view('neoView', [
-        //     'neoDayData' => $neoData['near_earth_objects'],
-        //     'element_count'=> $neoData['element_count'];
+        //     'asteroidList' => $asteroidList,
         //     'start_date'=>$start_date,
-        //     'end_date'=>$end_date
+        //     'end_date'=>$end_date,
+        //     'averageSizeKms'=>$averageSizeKms,
+        //     'fastestAsteroid' =>[
+        //         'id'=>$fastestAsteroidId,
+        //         'speedKmph'=> $fastestAsteroidSpeed,
+        //      ]
+        
+        //      'closestAsteroid' =>[
+        //        'id'=>$closestAsteroidId,
+        //        'distanceKms'=> $closestAsteroidDistanceKms,
+        //      ],
+        //      graphData=>$graphData
         // ]);
+        
         return view('neoView',['start_date'=>$start_date,'end_date'=>$end_date]);
 
 
--- resources/views/neoView.blade.php
+++ resources/views/neoView.blade.php
@@ -28,7 +28,7 @@
 	    <label class="visually-hidden" for="start_date">Start Date : </label>
 	    <div class="input-group">
 	      <div class="input-group-text">Start Date :</div>
-	      <input type="text" class="form-control" id="start_date" placeholder="start date" readonly />
+	      <input type="text" class="form-control " id="start_date" placeholder="start date" readonly />
 	    </div>
 	  </div>
 
@@ -50,15 +50,15 @@
 	<div class = "row">
 		<div class = "col-md-6">		
 			<form class = "border border-1">
-			  <label >Fastest Asteroid (km/h) :</label>
+			  <label ><h6>Fastest Asteroid</h6></label>
 			  <div class="form-outline mb-1">
-			    <label class="form-label" for="ID">ID : </label>
-			    <input type="text" id="ID" class="form-control" readonly />
+			    <label class="form-label" for="fastestAsteroidId">ID : </label>
+			    <input type="text" id="fastestAsteroidId" class="form-control" readonly />
 			  </div>
 
 			  <div class="form-outline mb-1">
-			    <label class="form-label" for="ID">Speed :</label>
-			    <input type="text" id="speed" class="form-control" readonly />
+			    <label class="form-label" for="fastestAsteroidSpeed">Speed(km/h) :</label>
+			    <input type="text" id="fastestAsteroidSpeed" class="form-control" readonly />
 			  </div>
 			</form>
 	
@@ -70,15 +70,15 @@
 	
 		<div class = "col-md-6">
 			<form class = "border border-1">
-			  <label >Closest Asteroid :</label>
+			  <label ><h6>Closest Asteroid</h6></label>
 			  <div class="form-outline mb-1 "> 
-			    <label class="form-label" for="ID">ID : </label>
-			    <input type="text" id="ID" class="form-control" readonly />
+			    <label class="form-label" for="closestAsteroidId">ID : </label>
+			    <input type="text" id="closestAsteroidId" class="form-control" readonly />
 			  </div>
 
 			  <div class="form-outline mb-1">
-			    <label class="form-label" for="distance">Distance(km) :</label>
-			    <input type="text" id="distance" class="form-control" readonly />
+			    <label class="form-label" for="closestAsteroidDistance">Distance(km) :</label>
+			    <input type="text" id="closestAsteroidDistance" class="form-control" readonly />
 			  </div>
 			</form>
 		</div>
@@ -97,39 +97,71 @@
  
 
 
-<canvas id="myChart" width="400" height="400"></canvas>
+<canvas id="myChart" width="200" height="200"></canvas>
 <script>
-const start_date = '<?php echo $start_date ?>';	
-const end_date = '<?php echo $end_date ?>';	
 
-console.log(start_date);
+//get the paramaters from data passed to view
+const start_date = '<?php echo $start_date ?>';	
+const end_date = '<?php echo $end_date ?>';
+const fastestAsteroidId = '<?php echo $fastestAsteroid['id'] ?>';
+const fastestAsteroidSpeed = '<?php echo $fastestAsteroid['speedKmph'] ?>';		
+const closestAsteroidId = '<?php echo $closestAsteroid['id'] ?>';
+const closestAsteroidDistance = '<?php echo $closestAsteroid['distanceKms'] ?>';
+const averageSizeKms = '<?php echo $averageSizeKms ?>';	
+const graphData = '<?php echo $graphData ?>';				
+
+//set field values
 document.getElementById("start_date").value = start_date;
 document.getElementById("end_date").value = end_date;
+document.getElementById("fastestAsteroidId").value =fastestAsteroidId;
+document.getElementById("fastestAsteroidSpeed").value =fastestAsteroidSpeed;
+document.getElementById("closestAsteroidId").value =closestAsteroidId;
+document.getElementById("closestAsteroidDistance").value =closestAsteroidDistance;
+document.getElementById("avg_size").value = averageSizeKms;
+
+
+
+//graph 
+graphData.forEach(createLabels);
+
+const dates = [];//labels for x-axis 'dates'
+const dayCounts= [];//data for y-axis 'dayCounts'
+
+//compute x-axis data
+function createLabels(item,index){
+	labels.push(item[0]);
+}
+
+//compute y-axis data
+function createData(item,index){
+	dates.push(item[1]);
+}
 
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: dates,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            label: '# of Asteroids',
+            data: dayCounts,
+            // backgroundColor: [
+            //     'rgba(255, 99, 132, 0.2)',
+            //     'rgba(54, 162, 235, 0.2)',
+            //     'rgba(255, 206, 86, 0.2)',
+            //     'rgba(75, 192, 192, 0.2)',
+            //     'rgba(153, 102, 255, 0.2)',
+            //     'rgba(255, 159, 64, 0.2)'
+            // ],
+            // borderColor: [
+            //     'rgba(255, 99, 132, 1)',
+            //     'rgba(54, 162, 235, 1)',
+            //     'rgba(255, 206, 86, 1)',
+            //     'rgba(75, 192, 192, 1)',
+            //     'rgba(153, 102, 255, 1)',
+            //     'rgba(255, 159, 64, 1)'
+            // ],
             borderWidth: 1
         }]
     },
